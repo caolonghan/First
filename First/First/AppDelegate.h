6721696ccf12284d1762e8d9e8b741b4d7516bef
Junjie1314 <85552027@qq.com>
2016-02-27 19:44:33 +0800

    creat a project

---
+++ First/First/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  First
+//
+//  Created by Qhf on 16/2/27.
+//  Copyright © 2016年 Junjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ First/First/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  First
+//
+//  Created by Qhf on 16/2/27.
+//  Copyright © 2016年 Junjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
